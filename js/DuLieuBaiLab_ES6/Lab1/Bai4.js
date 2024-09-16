ec37869cd8ca0d30f926bb142e15f3b6023eb2f5
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-09-16 21:09:55 +0700

    update js lab1

--- js/DuLieuBaiLab_ES6/Lab1/Bai4.js
+++ js/DuLieuBaiLab_ES6/Lab1/Bai4.js
@@ -1,43 +1,67 @@
-document.getElementById("user-form").addEventListener('submit', (event) => {
+let userList = [];
+
+let ul = document.getElementById("user-list");
+let userForm = document.getElementById("user-form");
+
+let nameElement = document.getElementById("name");
+let emailElement = document.getElementById("email");
+let ageElement = document.getElementById("age");
+
+function deleteUser(index) {
+    userList = userList.filter((_, i) => {
+        return i !== index;
+    });
+};
+
+function renderUser() {
+    ul.innerHTML = '';
+    userList.forEach((user, index) => {
+        let li = document.createElement('li');
+
+        let {name: name} = user;
+        let {email: email} = user;
+        let {age: age} = user;
+
+        let editButton = document.createElement('button');
+        let delButton = document.createElement('button');
+
+        editButton.appendChild(document.createTextNode("Sửa"));
+        delButton.appendChild(document.createTextNode("Xóa"));
+
+        editButton.addEventListener('click', () => {
+            let user = userList[index];
+            
+            nameElement.value = user.name;
+            ageElement.value = user.age;
+            emailElement.value = user.email;
+
+            deleteUser(index);
+        });
+
+        delButton.addEventListener('click', () => {
+            deleteUser(index);
+            renderUser();
+        });
+
+        li.appendChild(document.createTextNode(name));
+        li.appendChild(document.createTextNode(email));
+        li.appendChild(document.createTextNode(age + ' tuổi'));
+        li.appendChild(editButton);
+        li.appendChild(delButton);
+
+        ul.appendChild(li);
+    });
+}
+
+userForm.addEventListener('submit', (event) => {
     event.preventDefault();
-    let nameElement = document.getElementById("name");
-    let emailElement = document.getElementById("email");
-    let ageElement = document.getElementById("age");
 
     let name = nameElement.value;
     let email = emailElement.value;
     let age = ageElement.value;
 
-    let ul = document.getElementById("user-list");
+    userList = [...userList, { name, email, age }];
 
-    let li = document.createElement("li");
-
-    let editButton = document.createElement('button');
-    let delButton = document.createElement('button');
-
-    editButton.appendChild(document.createTextNode("Sửa"));
-    delButton.appendChild(document.createTextNode("Xóa"));
-
-    editButton.addEventListener('click', () => {
-        nameElement.value = name;
-        ageElement.value = age;
-        emailElement.value = email;
-        ul.removeChild(li);
-    });
-
-    delButton.addEventListener('click', () => {
-        ul.removeChild(li);
-    });
-
-    li.appendChild(document.createTextNode(name));
-    li.appendChild(document.createTextNode(email));
-    li.appendChild(document.createTextNode(age + ' tuổi'));
-    li.appendChild(editButton);
-    li.appendChild(delButton);
-
-    ul.appendChild(li);
-
-    nameElement.value = '';
-    ageElement.value = '';
-    emailElement.value = '';
+    renderUser();
+    userForm.reset();
 });
--- js/DuLieuBaiLab_ES6/Lab1/Bai5.js
+++ js/DuLieuBaiLab_ES6/Lab1/Bai5.js
@@ -1,60 +1,82 @@
 let ul = document.getElementById("book-list");
+let bookList = [];
 
+let bookForm = document.getElementById("book-form");
 let titleElement = document.getElementById("title");
 let authorElement = document.getElementById("author");
 let pagesElement = document.getElementById('pages');
 let totalPagesElement = document.getElementById("total-pages");
 
-function calc() {
-    let total = 0;
-    ul.querySelectorAll('li').forEach((li) => {
-        let page = +li.childNodes[2].textContent;
-        total += page;
+function deleteBook(index) {
+    bookList = bookList.filter((book, i) => { 
+        return i !== index; 
     });
+};
 
-    totalPagesElement.textContent = "Tổng số trang: " + total;
-}
+function renderBook() {
+    ul.innerHTML = '';
 
-document.getElementById("book-form").addEventListener('submit', (e) => {
+    bookList.forEach((book, index) => {
+        let li = document.createElement('li');
+
+        let {title: title} = book;
+        let {author: author} = book;
+        let {pages: pages} = book; 
+
+        let btnEdit = document.createElement('button');
+        let btnDel = document.createElement('button');
+
+        btnEdit.appendChild(document.createTextNode('Sửa'));
+        btnDel.appendChild(document.createTextNode('Xóa'));
+
+        btnEdit.addEventListener('click', () => {
+            let book = bookList[index];
+
+            titleElement.value = book.title;
+            authorElement.value = book.author;
+            pagesElement.value = book.pages;
+
+            deleteBook(index);
+        });
+
+        btnDel.addEventListener('click', () => {
+            deleteBook(index);
+            renderBook();
+        });
+
+        // add to li
+        li.appendChild(document.createTextNode(title));
+        li.appendChild(document.createTextNode(author));
+        li.appendChild(document.createTextNode(pages));
+        li.appendChild(btnEdit);
+        li.appendChild(btnDel);
+
+        ul.appendChild(li);
+    });
+    
+    let total = bookList.reduce((total, book) => {
+        let pages = +book.pages;
+        return total + pages;
+    }, 0);
+
+    totalPagesElement.textContent = `Tổng số trang: ${total}`;
+};
+
+bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     let {value: title} = titleElement;
     let {value: author} = authorElement;
     let {value: pages} = pagesElement;
 
-    let li = document.createElement('li');
+    if (isNaN(pages) || pages < 1) {
+        alert('Vui lòng nhập số trang hợp lệ.');
+        return;
+    };
 
-    // button
-    let btnEdit = document.createElement('button');
-    let btnDel = document.createElement('button');
+    bookList = [...bookList, { title, author, pages }];
 
-    btnEdit.appendChild(document.createTextNode('Sửa'));
-    btnDel.appendChild(document.createTextNode('Xóa'));
+    renderBook();
 
-    btnEdit.addEventListener('click', () => {
-        titleElement.textContent = title;
-        authorElement.textContent = author;
-        pagesElement.textContent = pages;
-
-        ul.removeChild(li);
-        calc();
-    });
-    btnDel.addEventListener('click', () => {
-        ul.removeChild(li);
-        calc();
-    });
-
-    // add to li
-    li.appendChild(document.createTextNode(title));
-    li.appendChild(document.createTextNode(author));
-    li.appendChild(document.createTextNode(pages));
-    li.appendChild(btnEdit);
-    li.appendChild(btnDel);
-
-    ul.appendChild(li);
-    calc();
-
-    titleElement.textContent = '';
-    authorElement.textContent = '';
-    pagesElement.textContent = '';
+    bookForm.reset();
 });
